f409eafd1e982d1a7aed75097163255bab1ba2fc
Harold Serrano <harold.serrano@gmail.com>
2014-09-25 13:02:35 -0700

    Setup the main classes
    Setup the main classes for blender

--- src/main.py
+++ src/main.py
@@ -5,7 +5,40 @@ Created on Sep 25, 2014
 '''
 import bpy
 
+class materials:
+    def __init__(self):
+        self.diffuse=[]
+        self.specular=[]
+        
+    
+class coordinates:
+    def __init__(self):
+        self.vertices=[]
+        self.normal=[]
+        self.uv=[]
+
+
+class textures:
+    def __init__(self):
+        self.texture
+
+class model:
+    def __init__(self):
+        self.localSpace=[]
+        self.absoluteSpace=[]
+    
+
+class lights:
+    pass
+
+class camera:
+    pass
+  
+
+
 def main():
+
+#bpy.context.scene.objects['Cube'].data.uv_layers.active.data[0].uv
 
     cube=bpy.data.objects["Cube"]
     print(cube.name)
