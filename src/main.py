16414439514b18263943ce53b63219888caa5f95
Harold Serrano <harold.serrano@untoldengine.com>
2018-09-08 17:25:21 -0700

    Added the license

--- src/main.py
+++ src/main.py
@@ -1,7 +1,22 @@
 '''
 Created on Sep 25, 2014
 
-Digital Asset Exporter script for the Untold Engine
+Digital Asset Exporter script for the Untold Engine. It extracts the attributes of a 3D model.
+
+Copyright (C) 2018  Untold Engine Studions
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 @author: Harold Serrano
 '''
